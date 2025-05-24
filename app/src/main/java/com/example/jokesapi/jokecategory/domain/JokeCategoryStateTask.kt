3c69ca17213bfac0f28c0cf3712d71eb97c0ba07
Ricky Lagerkvist <ricky@lifesum.com>
2025-05-24 15:40:59 +0200

    fix title

--- app/src/main/java/com/example/jokesapi/jokecategory/domain/JokeCategoryStateTask.kt
+++ app/src/main/java/com/example/jokesapi/jokecategory/domain/JokeCategoryStateTask.kt
@@ -1,11 +1,13 @@
 package com.example.jokesapi.jokecategory.domain
 
+import androidx.compose.ui.text.capitalize
 import com.example.jokesapi.R
 import com.example.jokesapi.data.JokesRepository
 import com.example.jokesapi.data.remote.model.JokesResultFailure
 import com.example.jokesapi.data.toUiModel
 import com.example.jokesapi.jokecategory.JokesCategoryContract
 import com.example.jokesapi.shared.model.JokeType
+import java.util.Locale
 import javax.inject.Inject
 
 class JokeCategoryStateTask @Inject constructor(
@@ -15,8 +17,9 @@ class JokeCategoryStateTask @Inject constructor(
         val result = repository.getJokesByType(type = jokeType)
         return if (result.isSuccessful && !result.data.isNullOrEmpty()) {
             val jokes = result.data.map { it.toUiModel() }
+            val title = jokes.first().jokeType.label.replaceFirstChar { it.titlecase(Locale.getDefault()) }
             JokesCategoryContract.State.DisplayJokes(
-                title = jokes.first().jokeType.name,
+                title = title,
                 jokes = jokes,
             )
         } else {
